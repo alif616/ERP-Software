bc2f52c965fd34aeba0a2b5281c23cd5a77504ab
alif616 <alif2580islam@gmail.com>
2026-05-04 05:40:23 +0600

    second commit

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use('/api/dashboard', require('./routes/dashboardRoutes'));
 app.use(require('./middleware/errorHandler'));
 
 // Server start
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
